365bbbe6133fb3646cfc1533b687dff55c9b0d0a
rudebwoy <patterson.carl95@gmail.com>
2015-12-30 18:43:13 -0500

    added second function to bubble sort

--- bubble.py
+++ bubble.py
@@ -1,11 +1,36 @@
 """Bubble sort algorithm"""
 
+
 def bubble(unsorted):
     sorted = False
+    l = len(unsorted) - 1
     while not sorted:
         sorted = True
-        for i in range(len(unsorted) -1):
+        for i in range(l):
             if unsorted[i] > unsorted[i+1]:
                 sorted = False
                 unsorted[i], unsorted[i+1] = unsorted[i+1], unsorted[i]
+    return unsorted
 
+
+def get_user_list():
+    user_list = list()
+    print('Enter your numbers: ')
+    t = False
+    while not t:
+        hold = input('> ')
+        if hold == '':
+            break
+        try:
+            float(hold)
+            user_list.append(float(hold))
+        except ValueError:
+            t = False
+            print('Enter numbers only!')
+
+    return user_list
+
+
+unsorted = get_user_list()
+print(unsorted)
+print(bubble(unsorted))
