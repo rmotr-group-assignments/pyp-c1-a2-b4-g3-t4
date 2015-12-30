95825ac151bfeb1b3ec46103934e244cb245c175
Carl Anthony <patterson.carl95@gmail.com>
2015-12-30 18:55:17 -0500

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -3,10 +3,9 @@
 
 def bubble(unsorted):
     sorted = False
-    l = len(unsorted) - 1
     while not sorted:
         sorted = True
-        for i in range(l):
+        for i in range(len(unsorted) -1):
             if unsorted[i] > unsorted[i+1]:
                 sorted = False
                 unsorted[i], unsorted[i+1] = unsorted[i+1], unsorted[i]
