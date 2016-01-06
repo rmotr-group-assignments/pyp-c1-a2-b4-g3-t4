644d32e8ac3c370ec3361e5d924ce88d40d55f6a
rudebwoy <patterson.carl95@gmail.com>
2016-01-05 19:29:01 -0500

    added some improvements to the code

--- bubble.py
+++ bubble.py
@@ -1,31 +1,37 @@
 """Bubble sort algorithm"""
 
 
-def bubble(unsorted):
-    sorted = False
-    l = len(unsorted) - 1
-    while not sorted:
-        sorted = True
-        for i in range(l):
-            if unsorted[i] > unsorted[i+1]:
-                sorted = False
-                unsorted[i], unsorted[i+1] = unsorted[i+1], unsorted[i]
-    return unsorted
+def bubble(unsorted_list):
+    """sorts the list"""
+
+    new_list = unsorted_list
+    s = False
+    while not s:
+        s = True
+        for i in range(len(unsorted_list) - 1):
+            if new_list[i] > new_list[i+1]:
+                s = False
+                new_list[i], new_list[i+1] = new_list[i+1], new_list[i]
+    return new_list
 
 
 def get_user_list():
+    """get and verify that input is the correct type"""
+
     user_list = list()
-    print('Enter your numbers: ')
+    print('Enter your numbers')
     t = False
     while not t:
         hold = input('> ')
+
         if hold == '':
             break
+
+        # checks if hold is a number
         try:
             float(hold)
             user_list.append(float(hold))
         except ValueError:
-            t = False
             print('Enter numbers only!')
 
     return user_list
